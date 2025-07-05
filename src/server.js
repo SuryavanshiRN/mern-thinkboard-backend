8f041d77fb270db37d416a2cffbe5d369b0f6a43
SuryavanshiRN <suryavanshiaryan58@gmail.com>
2025-07-05 19:35:52 +0530

    Removed static frontend serve for Render deployment

--- src/server.js
+++ src/server.js
@@ -34,11 +34,11 @@ app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use("/api/notes", notesRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../../frontend", "dist", "index.html"))
+);
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
 }
 
 connectDB().then(() => {
